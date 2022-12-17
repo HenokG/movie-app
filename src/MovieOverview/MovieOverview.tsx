0bc610bedf3197c94be9623c6a7ad50d3a8f21d6
Henok <hhenok1987@gmail.com>
2022-12-17 12:57:26 +0100

    feat: add antd for ui

---
+++ src/MovieOverview/MovieOverview.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+  title: any;
+}
+
+const MovieOverview: FunctionComponent<Props> = ({ title }) => {
+  return <div>Movie {title}</div>;
+};
+
+export default MovieOverview;
